ae3e14786c6786db771c6b0f3337a99bae8c3c23
Venky123007 <gollapallivenky123@gmail.com>
2023-11-03 15:18:04 +0530

    rechanged the api url

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_BASE_URL = "hissing-wren-production.up.railway.app/"
+export const API_BASE_URL = "https://hissing-wren-production.up.railway.app/"
 
 const jwt = localStorage.getItem("jwt");
 
