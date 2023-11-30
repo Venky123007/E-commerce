460797d269248e5090d02a4fd319136ea55aba2a
Venky123007 <gollapallivenky123@gmail.com>
2023-11-30 11:16:46 +0530

    Render Hosting

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_BASE_URL = "https://hissing-wren-production.up.railway.app/"
+export const API_BASE_URL = "https://aira-fashions.onrender.com/"
 //export const API_BASE_URL = "http://localhost:3007/"
 
 const jwt = localStorage.getItem("jwt");
